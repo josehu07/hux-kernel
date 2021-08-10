96641d90603ee37fed8c210d3d78c79a0111c5bf
Jose <huguanzhou123@sina.com>
2021-08-10 15:14:26 +0800

    started implementing hard disk driver

---
+++ src/device/idedisk.c
@@ -0,0 +1,10 @@
+/**
+ * Parallel ATA (IDE) hard disk driver.
+ * Assumes only port I/O (PIO) mode without DMA for now.
+ */
+
+
+#include "idedisk.h"
+
+
+//TODO
---
+++ src/device/idedisk.h
@@ -0,0 +1,14 @@
+/**
+ * Parallel ATA (IDE) hard disk driver.
+ * Assumes only port I/O (PIO) mode without DMA for now.
+ */
+
+
+#ifndef IDEDISK_H
+#define IDEDISK_H
+
+
+// TODO
+
+
+#endif
